88465e599cecccef81842855b5f12b8ed574a413
Commit de sofia <sanchezvsofia@gmail.com>
2018-05-13 15:40:07 -0300

    El throws de mierda

--- Repository/src/tda/TDABusiness.java
+++ Repository/src/tda/TDABusiness.java
@@ -1,9 +1,10 @@
 package tda;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface TDABusiness extends Remote {
 
-	public void crearCliente(String cuit, String razon_social, String telefono, String direccion, boolean r_inscripto, String notas);
+	public void crearCliente(String cuit, String razon_social, String telefono, String direccion, boolean r_inscripto, String notas) throws RemoteException;
 	
 }
