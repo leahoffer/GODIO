92a3db8c41137d23f5e2dc5c9549aacab0e86839
Commit de sofia <sanchezvsofia@gmail.com>
2018-05-13 17:13:32 -0300

    Herencia Condicion/Descuento/Bonificacion

---
+++ Server/src/business/Bonificacion.java
@@ -0,0 +1,12 @@
+package business;
+
+public class Bonificacion extends Condicion {
+
+	
+	private float monto;
+	public float calcularMonto(int montoInicial) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Server/src/business/Descuento.java
@@ -0,0 +1,12 @@
+package business;
+
+public class Descuento extends Condicion {
+
+	
+	private float porcentaje;
+	public float calcularMonto(int montoInicial) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
