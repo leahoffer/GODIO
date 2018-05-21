852fc7e962e2b2180b0061543c364e89ef105655
Leandro.Hoffer <leandrohoffer@live.com>
2018-05-21 10:39:33 -0300

    Toqueteado el recuperar Cliente

--- Server/src/controller/Controller.java
+++ Server/src/controller/Controller.java
@@ -3,18 +3,22 @@ package controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import business.Bonificacion;
 import business.Cliente;
 import business.Condicion;
 import business.CuentaCorriente;
+import business.Descuento;
 import business.DetallePedido;
 import business.Pedido;
 import business.Producto;
 import dao.ClienteDAO;
 import dao.PedidoDAO;
 import dao.ProductoDAO;
+import dto.BonificacionDTO;
 import dto.ClienteDTO;
 import dto.CondicionDTO;
 import dto.CuentaCorrienteDTO;
+import dto.DescuentoDTO;
 import dto.ProductoDTO;
 import entity.ClienteEntity;
 import entity.CondicionEntity;
@@ -45,21 +49,30 @@ public class Controller {
 		/**
 		 * Creo un método para que me deje crear el RO y esas otras cosas
 		 */
-		Cliente cliente=new Cliente();
-		CuentaCorriente cc= new CuentaCorriente();
-		cc.setId(c.getCuentaCorriente().getId());
-		cc.setLimite(c.getCuentaCorriente().getLimite());
-		cc.setSaldo(c.getCuentaCorriente().getSaldo());
+		try {
+			if(ClienteDAO.getInstance().findByCuit(c.getCuit()) == null)
+			{
+				Cliente cliente=new Cliente();
+				CuentaCorriente cc= new CuentaCorriente();
+				cc.setId(c.getCuentaCorriente().getId());
+				cc.setLimite(c.getCuentaCorriente().getLimite());
+				cc.setSaldo(c.getCuentaCorriente().getSaldo());
+				
+				cliente.setCondicionEsp(c.getCondicionEsp());
+				cliente.setCuentaCorriente(cc);
+				cliente.setCuit(c.getCuit());
+				cliente.setDireccion(c.getDireccion());
+				cliente.setR_inscripto(c.isR_inscripto());
+				cliente.setRazon_social(c.getRazon_social());
+				cliente.setTelefono(c.getTelefono());
+				
+				cliente.saveOrUpdate();
+			}
+		} catch (ClienteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		cliente.setCondicionEsp(c.getCondicionEsp());
-		cliente.setCuentaCorriente(cc);
-		cliente.setCuit(c.getCuit());
-		cliente.setDireccion(c.getDireccion());
-		cliente.setR_inscripto(c.isR_inscripto());
-		cliente.setRazon_social(c.getRazon_social());
-		cliente.setTelefono(c.getTelefono());
-		
-		cliente.saveOrUpdate();
 	}
 	
 	public void crearPedido (String cuit , ArrayList<DetallePedido> ped) throws ClienteException, ProductoException
@@ -117,14 +130,25 @@ public class Controller {
 		ClienteDTO cli = new ClienteDTO();
 		CuentaCorrienteDTO cta = new CuentaCorrienteDTO();
 		List<CondicionDTO> conds = new ArrayList<CondicionDTO>(); 
-		CondicionDTO con = new CondicionDTO();
-		/** Comento esto para que no putee por ahora, porque solo falta ver la herencia. Después se puede descomentar
-		for (Condicion ce :cliente.getCuentaCorriente().getCondiciones())
+		/** Comento esto para que no putee por ahora, porque solo falta ver la herencia. Después se puede descomentar**/
+		for (Condicion c :cliente.getCuentaCorriente().getCondiciones())
 		{
-			con.setCondicion(ce.getDescripcion());
-			conds.add(con);
+			if (c instanceof Bonificacion)
+			{
+				BonificacionDTO b = new BonificacionDTO();
+				b.setCondicion(c.getCondicion());
+				b.setMonto(((Bonificacion) c).getMonto());
+				conds.add(b);
+			}
+			else if (c instanceof Descuento)
+			{
+				DescuentoDTO d = new DescuentoDTO();
+				d.setCondicion(c.getCondicion());
+				d.setPorcentaje(((Descuento) c).getPorcentaje());
+				conds.add(d);
+			}
 		}
-		**/	
+		
 		cta.setCondiciones(conds);
 		cli.setCondicionEsp(cliente.getCondicionEsp());
 		cli.setCuentaCorriente(cta);
@@ -133,8 +157,6 @@ public class Controller {
 		cli.setR_inscripto(cli.isR_inscripto()); 
 		cli.setRazon_social(cli.getRazon_social());
 		cli.setTelefono(cli.getTelefono());
-		
-		
 		clientes.add(cli);
 		return cli;
 		
